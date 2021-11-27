efc42cd0f66a1ed0040f536489832f6e69a287d0
GyRG <gyorgyluisr@gmail.com>
2021-11-26 19:33:42 -0600

    Realizando las primeras correcciones para los ejercicios de la Tarea_01

--- Tarea_01/T1E1_pos_neg.py
+++ Tarea_01/T1E1_pos_neg.py
@@ -2,16 +2,22 @@
 Date:       2021-11-12
 Authors:    Jorge Luis Rodríguez Guzmán
 File:       T1E1_pos_neg.py
-Brief:      Ingresar un número y validar 
+Brief:      Ingresar un número y validar
             si es un número positivo, negativo o cero.
-Score:      -
-Versión:    0.0.1
-Fixes:      -
+Score:      65
+Version:    0.1.2
+Fixes:      - Falta la condición de __main__
+            - PEP8 recomienda no dejar espacio en blanco entre la
+                condición de if y el carácter de dos puntos ':'
+            - PEP8 recomienda no dejar espacio en blanco entre printf
+                y la apertura del paréntesis '('
 """
-num = int(input("Ingresa un número \n"))
-if num < 0 :
-    print (f"{num} es negativo ")
-if num > 0 :
-    print (f"{num} es positivo ")
-if num == 0 :
-    print (f"{num} es neutro ")
+
+if __name__=="__main__":
+    num=float(input("Ingresa un número \n"))
+    if num<0:
+        print(f"{num} es negativo ")
+    if num>0:
+        print(f"{num} es positivo ")
+    if num==0:
+        print(f"{num} es neutro ")
