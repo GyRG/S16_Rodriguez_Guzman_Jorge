872fea4890c0882669ec0804eda46fd7702a1651
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-12-04 01:13:10 -0600

    Update T1E2_even_odd.py

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -3,15 +3,22 @@ Date:       2021-11-12
 Authors:    Jorge Luis Rodríguez Guzmán
 File:       T1E2_even_odd.py
 Brief:      Ingresar un número y validar si es par o impar.
-Score:      -
-Versión:    0.0.2
-Fixes:      -
- """
+Score:      85
+Versión:    1.1.1
+Fixes:      - Falta la condición de __main__
+
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores 
+"""
+
+# BIEN DECLARADO LAS CONSTANTES A COMO SE ESPECIFICÓ EN LOS REQUISITOS
+# PERO ES MEJOR USAR TUPLAS
 FREQ = 2
 MODULO_ESPERADO = 0
 
-num=int(input("Ingresar un número: \n"))
-if num%FREQ!=MODULO_ESPERADO:
+num=int(input("Ingresar un número: \n"))    # PEP8
+# SERIA BUENO VALIDAR E INDICAR CUANDO EL NÚMERO SEA CERO
+if num%FREQ!=MODULO_ESPERADO:               # PEP8 PEP8
     print(f"{num} es impar")
 else:
     print(f"{num} es par")
